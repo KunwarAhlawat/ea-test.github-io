d704fb657fbd5f77a4f8ba7af7710c9bae188c34
UdayAhlawat49 <udayahlawat017@gmail.com>
2024-01-26 11:49:08 +0530

    uday commit

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ app.set('views' , path.join(__dirname, 'views'))
 const indexRoute = require("./routes/indexRoute")
 const courseRoute = require("./routes/courseRoute")
 const courseViewRoute = require("./routes/courseViewRoute")
+const adminRoute = require('./routes/adminRoute');
 const notFoundRoute = require("./routes/notFoundRoute")
 app.use("/" , indexRoute)
 app.use("/" , courseRoute)
 app.use("/" , courseViewRoute)
 app.use("/" , notFoundRoute)
-
+app.use('/', adminRoute);
 const port = 3000
 
 app.listen(port , ()=>{
